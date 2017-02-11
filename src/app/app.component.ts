b43a4b210ba8cf49b90955b470e7e0f207cb8025
lala.lee <lala.lee.goo@gmail.com>
2017-02-11 13:47:08 +0800

    任務 06：套用事件繫結 (Event Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   inputHint:string = 'Please input your todo item';
+  todos:any[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addTodo(todoval:string){
+    this.todos.push(todoval);
+    console.log(this.todos);
+  }
+
 }
