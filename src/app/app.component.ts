6685b6fb9634a12967c3ba5ffd82b80f7d517e0f
lala.lee <lala.lee.goo@gmail.com>
2017-02-11 13:36:30 +0800

    套用屬性繫結 (Property Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  inputHint:string = 'Please input your todo item';
 }
